1bb9b22bbafcb626ec2c01be203dcc4d06c9e436
Ajay <podiliajay14@gmail.com>
2024-04-01 18:02:03 +0530

    CDN to React package

--- Day01/SampleProject2/App1.js
+++ Day01/SampleProject2/App1.js
@@ -1,8 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const parent = React.createElement("div",{id:"parent"}, 
                     [React.createElement("div",{id:"child"},[
                         React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am H2 tag")]),
                         React.createElement("div",{id:"child2"},[
-                            React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am H2 tag")])])
+                            React.createElement("h1",{},"I am h1 tag"),React.createElement("h2",{},"I am H2 tag")])]);
+console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"))
-console.log(parent)
 root.render(parent);
