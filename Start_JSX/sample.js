a6622f60dff9047795acf5f60de23f7bd51967de
Ajay <podiliajay14@gmail.com>
2024-04-20 19:19:38 +0530

    IntoJSX

---
+++ Start_JSX/sample.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom"
+
+const jsxHeading = <h1 id="heading">I am from JSX</h1>
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(jsxHeading);
